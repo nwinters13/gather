dc76695c41b8d648b9582fcd23cd8099bb117fde
nwinters13 <nathan.winters@tufts.edu>
2014-10-25 00:58:01 -0400

    poo

--- index.js
+++ index.js
@@ -59,20 +59,18 @@ app.post('/invitePerson', function(req, res) {
 				res.send(array[0]);		
 				var name = req.body.user;
 				var user = collection.find({"user": name}).toArray(function (err, r){
-					//res.send(r);
 					if (err) {
 						collection.insert({"user": name}, function (err, r){
-						//	res.send(r[0].user);
-						});
 						res.send(200);
+						});
 					} else {
 						var invites = r[0].invited;
 						invites.push(req.body.eventID);
 					 	collection.update({"user": r[0].user}, {"user": r[0].user, "invited": invites, "accepted": r[0].accepted});
-						res.send(201);
+						res.send(202);
 					}
 				});
-				//res.send(200);
+				res.send(204);
 			});
 		});
 	});  
