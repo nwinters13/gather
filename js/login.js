f43f1eb210e16233d9ee08eef491acf5eb8302f2
schurc01 <sjchurch18@gmail.com>
2014-10-25 03:01:01 -0400

    it's a longshot

--- js/login.js
+++ js/login.js
@@ -41,6 +41,7 @@
     // for FB.getLoginStatus().
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
+      location.reload();
        window.location = "mainpage.html";
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
