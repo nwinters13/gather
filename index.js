363e1da554d4bbcfe22485d0a31525a3fe3b06e3
nwinters13 <nathan.winters@tufts.edu>
2014-10-25 01:04:39 -0400

    poo

--- index.js
+++ index.js
@@ -55,23 +55,21 @@ app.post('/submit.json', function (req, res) {
 app.post('/invitePerson', function(req, res) {
 	mongo.Db.connect(mongoURI, function (err, db) {
 		db.collection ("users", function (er, collection) {
-			collection.find({}).sort().toArray(function (err, array) {		
-				var name = req.body.user;
-				var user = collection.find({user: name}).toArray(function (err, r){
-					if (err) {
-						collection.insert({"user": name}, function (err, r){
-							res.send(200);
-						});
+			var name = req.body.user;
+			var user = collection.find({user: name}).toArray(function (err, r){
+				if (err) {
+					collection.insert({"user": name}, function (err, r){
 						res.send(200);
-					} else {
-						var invites = r.invited;
-						invites.push(req.body.eventID);
-					 	collection.update({"user": r.user}, {"user": r.user, "invited": invites, "accepted": r.accepted});
-						res.send(202);
-					}
-				});
-				res.send(204);
+					});
+					res.send(200);
+				} else {
+					var invites = r.invited;
+					invites.push(req.body.eventID);
+				 	collection.update({"user": r.user}, {"user": r.user, "invited": invites, "accepted": r.accepted});
+					res.send(202);
+				}
 			});
+			res.send(204);
 		});
-	});  
-});
+	});
+});  
