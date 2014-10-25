04404e4b500eab40c3f46e2f096643508576e1f1
SophieDasinger <sophie.dasinger@gmail.com>
2014-10-25 08:15:03 -0400

    hi

--- js/account.js
+++ js/account.js
@@ -16,7 +16,7 @@
 
 var form = document.getElementsByClassName("form-control");
 var button = document.getElementsByClassName("input-group-btn");
-button.onClick = sendRequest();
+button.onclick = sendRequest();
 
 function sendRequest() {
 	var request = new XMLHttpRequest();
