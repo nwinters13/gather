425fbdbba831f25335c7a63cb41f88e9a1ca5eec
nwinters13 <nathan.winters@tufts.edu>
2014-10-24 21:34:13 -0400

    figuring out depends

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/', function(request, response) {
 });
 
 app.get('/index.html', function(req, res) {
-  res.send(mongoURI);
+  res.send(db);
 });
 
 app.listen(app.get('port'), function() {
