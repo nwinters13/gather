1aa7441a06a03bd20bd0344e3a81d38019a002d7
schurc01 <sjchurch18@gmail.com>
2014-10-25 02:47:45 -0400

    IT WORKS FOR OTHER PEOPLE

--- js/login.js
+++ js/login.js
@@ -35,6 +35,7 @@
   // This is called with the results from from FB.getLoginStatus().
   function statusChangeCallback(response) {
     console.log(response);
+    window.location.reload();
     // The response object is returned with a status field that lets the
     // app know the current login status of the person.
     // Full docs on the response object can be found in the documentation
