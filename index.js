76d772427050d8d18dcf1c0aa81da8f8aaca97d2
nwinters13 <nathan.winters@tufts.edu>
2014-10-24 21:06:30 -0400

    figuring out depends

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 var path = require('path');
 
 var mongo = require('mongodb');
-var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
 var json = require('json');
 var express = require('express')
 var app = express();
