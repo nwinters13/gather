3ca169363c95ed574bd5746112a524bf9027a52c
schurc01 <sjchurch18@gmail.com>
2014-10-25 06:35:31 -0400

    poop

--- js/myjs.js
+++ js/myjs.js
@@ -1,34 +1,18 @@
 var map;
 var places;
 var myPlaces;
-var hasLoaded = false;
 function initialize() {
   var mapOptions = {
     zoom: 8,
     center: new google.maps.LatLng(42.4055470, -71.1238240)
   };
-  if (!hasLoaded) {
- 	 map = new google.maps.Map(document.getElementById('map-canvas'),
-    	  mapOptions);
-    	setupMap();
-    	hasLoaded = true;
-  }
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+    setupMap();
 }
 
-//google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initialize);
 
-
-function loadScript() {
-
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?sensor=true&libraries=places&' +
-      'callback=initialize';
-  document.body.appendChild(script);
-  hasLoaded = true;
-}
-
-window.onload = loadScript();
 
 function setupMap()
 {
@@ -40,28 +24,22 @@ function setupMap()
 			};
 	service = new google.maps.places.PlacesService(map);
 	service.search(request, callback);
-	alert('setting up');
 }
 // Taken from http://code.google.com/apis/maps/documentation/javascript/places.html
 function callback(results, status)
 {
 	if (status == google.maps.places.PlacesServiceStatus.OK) {
-		alert("hi");
 		alert("Got places back!");
-		alert('phew');
 		places = results;
-		alert('lol');
 		console.log("testing status");
 		for (var i = 0; i < 6; i++) {
 			console.log(results[i]);
 			createMarker(results[i]);
 			console.log("created an marker");
-			alert(i);
+
 		}
 	}
-	alert('calling load data');
 	loadData();
-	alert('done load data');
 }
 function createMarker(place)
 {
@@ -82,17 +60,14 @@ function createMarker(place)
 
 function loadData() 
 {
-	alert('in load data');
 	myPlaces = document.getElementById("places").getElementsByClassName("list-group-item");
 	for(var i =0; i < 6; i++) {
 			myPlaces[i].innerHTML = places[i].name;
 	}
-	alert('alert');
 	var myFriends = document.getElementById("people").getElementsByClassName("list-group-item");
 	for(var i =0; i < myFriends.length; i++) {
-		alert("myfriends " + i);
 		// do something with myFriends[
 	}
-	alert('leaving!');
+
 }
 
