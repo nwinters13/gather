c8dd3f05f464c7e2cd62189682f8a84d418b0e04
nwinters13 <nathan.winters@tufts.edu>
2014-10-25 03:04:48 -0400

    user gets

--- index.js
+++ index.js
@@ -180,12 +180,23 @@ app.post('/accept', function(req, res) {
 	});
 }); 
 
-app.get('/currentInvites', function (req, res) {
+app.get('/currentInvited', function (req, res) {
 	mongo.Db.connect(mongoURI, function (err, db) {
 		db.collection("users", function (er, collection) {
 			var name = req.query.user;
 			var user = collection.find({user: name}).toArray(function (err, r) {
 				res.send(r[0].invited);
+			});
+		});
+	});
+});
+
+app.get('/currentAccepted', function (req, res) {
+	mongo.Db.connect(mongoURI, function (err, db) {
+		db.collection("users", function (er, collection) {
+			var name = req.query.user;
+			var user = collection.find({user: name}).toArray(function (err, r) {
+				res.send(r[0].accepted);
 			});
 		});
 	});
