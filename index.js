cca0e5821005915e48c238ee299840611a450e8a
nwinters13 <nathan.winters@tufts.edu>
2014-10-24 21:16:04 -0400

    figuring out depends

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var lobbyDB = mongoose.connection;
 
 
  var newGoose = require('mongoose');
-// var userURI = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://http://gatherup.herokuapp.com/users';
+ var userURI = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://http://gatherup.herokuapp.com/users';
 // newGoose.connect(userURI);
 // var userDB = newGoose.connection;
 // var userDB = mongo.Db.connect(userURI, function(err, dbConn) {
