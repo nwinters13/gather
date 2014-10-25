837e94bbacd5cb6a7f2c99bc8cc7dc10dd3dfc62
nwinters13 <nathan.winters@tufts.edu>
2014-10-25 04:35:28 -0400

    create event

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.post('/invitePerson', function(req, res) {
 
 app.post('/login', function(req, res) {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header("Access-Control-Allow-Headers", "*");
 	mongo.Db.connect(mongoURI, function (err, db) {
 		db.collection ("users", function (er, collection) {
 			var name = req.body.user;
