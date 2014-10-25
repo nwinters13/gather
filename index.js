ca56f259de2b3850d620e1afd35197ea6ebb6828
nwinters13 <nathan.winters@tufts.edu>
2014-10-25 05:39:49 -0400

    create event

--- index.js
+++ index.js
@@ -338,8 +338,8 @@ app.get('/midpoint', function (req, res) {
 				var avgLng = 0;
 				var LatLng = new Array();
 				for (j = 0; j < numUsers; j++) {
-					avgLat += r[0].lats[j];
-					avgLng += r[0].lngs[j];
+					avgLat += parseFloat(r[0].lats[j]);
+					avgLng += parseFloat(r[0].lngs[j]);
 				}
 				res.send(avgLat);
 				avgLat = avgLat/numUsers;
